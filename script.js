c541533b858ad72d963ecbb2dc8c3dd5616b9b8c
Rick-Pacheco <rpvaranda@gmail.com>
2023-12-31 15:32:29 -0300

    alterando a função changeCurrency e juntando os dois Selects

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const btnConvertCurrency = document.querySelector(".convert-button")
-const SelectCurrencySelect1 = document.querySelector(".currency-select1")
-const SelectCurrencySelect2 = document.querySelector(".currency-select2")
+const selectCurrencySelect1 = document.querySelector(".currency-select1")
+const selectCurrencySelect2 = document.querySelector(".currency-select2")
 
 function convertCurrency() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
@@ -14,7 +14,7 @@ function convertCurrency() {
     //        (REFERÊNCIA ---  MOEDA REAL PARA TODOS!)
 
     //            Formatação de "Real", para "..."
-    if (SelectCurrencySelect1.value == "real" && SelectCurrencySelect2.value == "real") {
+    if (selectCurrencySelect1.value == "real" && selectCurrencySelect2.value == "real") {
 
         pCurrencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
             style: "currency",
@@ -28,7 +28,7 @@ function convertCurrency() {
     }
 
 
-    if (SelectCurrencySelect1.value == "real" && SelectCurrencySelect2.value == "dolar") {
+    if (selectCurrencySelect1.value == "real" && selectCurrencySelect2.value == "dolar") {
         pCurrencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
             style: "currency",
             currency: "BRL"
@@ -40,7 +40,7 @@ function convertCurrency() {
         }).format(inputCurrencyValue / dolarToday)
     }
 
-    if (SelectCurrencySelect1.value == "real" && SelectCurrencySelect2.value == "euro") {
+    if (selectCurrencySelect1.value == "real" && selectCurrencySelect2.value == "euro") {
         pCurrencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
             style: "currency",
             currency: "BRL"
@@ -52,7 +52,7 @@ function convertCurrency() {
         }).format(inputCurrencyValue / euroToday)
     }
 
-    if (SelectCurrencySelect1.value == "real" && SelectCurrencySelect2.value == "libra") {
+    if (selectCurrencySelect1.value == "real" && selectCurrencySelect2.value == "libra") {
         pCurrencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
             style: "currency",
             currency: "BRL"
@@ -65,7 +65,7 @@ function convertCurrency() {
     }
 
     //            Formatação de "Dolar", para "..."
-    if (SelectCurrencySelect1.value == "dolar" && SelectCurrencySelect2.value == "dolar") {
+    if (selectCurrencySelect1.value == "dolar" && selectCurrencySelect2.value == "dolar") {
         pCurrencyValueToConvert.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD"
@@ -76,7 +76,7 @@ function convertCurrency() {
             currency: "USD"
         }).format(inputCurrencyValue)
     }
-    if (SelectCurrencySelect1.value == "dolar" && SelectCurrencySelect2.value == "real") {
+    if (selectCurrencySelect1.value == "dolar" && selectCurrencySelect2.value == "real") {
         pCurrencyValueToConvert.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD"
@@ -88,7 +88,7 @@ function convertCurrency() {
         }).format(inputCurrencyValue * dolarToday)
     }
 
-    if (SelectCurrencySelect1.value == "dolar" && SelectCurrencySelect2.value == "euro") {
+    if (selectCurrencySelect1.value == "dolar" && selectCurrencySelect2.value == "euro") {
         pCurrencyValueToConvert.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD"
@@ -100,7 +100,7 @@ function convertCurrency() {
         }).format(inputCurrencyValue * (dolarToday / euroToday))
     }
 
-    if (SelectCurrencySelect1.value == "dolar" && SelectCurrencySelect2.value == "libra") {
+    if (selectCurrencySelect1.value == "dolar" && selectCurrencySelect2.value == "libra") {
         pCurrencyValueToConvert.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD"
@@ -114,7 +114,7 @@ function convertCurrency() {
     
 
     //            Formatação de "Euro", para "..."
-    if (SelectCurrencySelect1.value == "euro" && SelectCurrencySelect2.value == "euro") {
+    if (selectCurrencySelect1.value == "euro" && selectCurrencySelect2.value == "euro") {
         pCurrencyValueToConvert.innerHTML = new Intl.NumberFormat("de-De", {
             style: "currency",
             currency: "EUR"
@@ -126,7 +126,7 @@ function convertCurrency() {
         }).format(inputCurrencyValue)
     }
 
-    if (SelectCurrencySelect1.value == "euro" && SelectCurrencySelect2.value == "real") {
+    if (selectCurrencySelect1.value == "euro" && selectCurrencySelect2.value == "real") {
         pCurrencyValueToConvert.innerHTML = new Intl.NumberFormat("de-De", {
             style: "currency",
             currency: "EUR"
@@ -138,7 +138,7 @@ function convertCurrency() {
         }).format(inputCurrencyValue * euroToday)
     }
 
-    if (SelectCurrencySelect1.value == "euro" && SelectCurrencySelect2.value == "dolar") {
+    if (selectCurrencySelect1.value == "euro" && selectCurrencySelect2.value == "dolar") {
         pCurrencyValueToConvert.innerHTML = new Intl.NumberFormat("de-De", {
             style: "currency",
             currency: "EUR"
@@ -150,7 +150,7 @@ function convertCurrency() {
         }).format(inputCurrencyValue * (euroToday / dolarToday))
     }
 
-    if (SelectCurrencySelect1.value == "euro" && SelectCurrencySelect2.value == "libra") {
+    if (selectCurrencySelect1.value == "euro" && selectCurrencySelect2.value == "libra") {
         pCurrencyValueToConvert.innerHTML = new Intl.NumberFormat("de-De", {
             style: "currency",
             currency: "EUR"
@@ -163,7 +163,7 @@ function convertCurrency() {
     }
 
     //            Formatação de "Libra", para "..."
-    if (SelectCurrencySelect1.value == "libra" && SelectCurrencySelect2.value == "libra") {
+    if (selectCurrencySelect1.value == "libra" && selectCurrencySelect2.value == "libra") {
         pCurrencyValueToConvert.innerHTML = new Intl.NumberFormat("en-Gb", {
             style: "currency",
             currency: "GBP"
@@ -175,7 +175,7 @@ function convertCurrency() {
         }).format(inputCurrencyValue)
     }
 
-    if (SelectCurrencySelect1.value == "libra" && SelectCurrencySelect2.value == "real") {
+    if (selectCurrencySelect1.value == "libra" && selectCurrencySelect2.value == "real") {
         pCurrencyValueToConvert.innerHTML = new Intl.NumberFormat("en-Gb", {
             style: "currency",
             currency: "GBP"
@@ -187,7 +187,7 @@ function convertCurrency() {
         }).format(inputCurrencyValue * libraToday)
     }
 
-    if (SelectCurrencySelect1.value == "libra" && SelectCurrencySelect2.value == "dolar") {
+    if (selectCurrencySelect1.value == "libra" && selectCurrencySelect2.value == "dolar") {
         pCurrencyValueToConvert.innerHTML = new Intl.NumberFormat("en-Gb", {
             style: "currency",
             currency: "GBP"
@@ -199,7 +199,7 @@ function convertCurrency() {
         }).format(inputCurrencyValue * (libraToday / dolarToday))
     }
 
-    if (SelectCurrencySelect1.value == "libra" && SelectCurrencySelect2.value == "euro") {
+    if (selectCurrencySelect1.value == "libra" && selectCurrencySelect2.value == "euro") {
         pCurrencyValueToConvert.innerHTML = new Intl.NumberFormat("en-Gb", {
             style: "currency",
             currency: "GBP"
@@ -215,58 +215,62 @@ function convertCurrency() {
 }
 
 
-//           TROCA DO PRIMEIRO SELECT
+//                     TROCA DO SELECT
 
-function changeCurrency1() {
+function changeCurrency() {
+
     const pCurrencyName1 = document.getElementById("currency-name1")
     const imageCurrencyImg1 = document.querySelector(".currency-img1")
+    const pCurrencyName2 = document.getElementById("currency-name2")
+    const imageCurrencyImg2 = document.querySelector(".currency-img2")
 
-    if (SelectCurrencySelect1.value == "dolar") {
+       //                 Primeiro Select
+
+    if (selectCurrencySelect1.value == "dolar") {
         pCurrencyName1.innerHTML = "Dólar Americano"
         imageCurrencyImg1.src = "./assets/dollar.png"
     }
-    if (SelectCurrencySelect1.value == "euro") {
+
+    if (selectCurrencySelect1.value == "euro") {
         pCurrencyName1.innerHTML = "Euro"
         imageCurrencyImg1.src = "./assets/euro.png"
     }
-    if (SelectCurrencySelect1.value == "libra") {
+
+    if (selectCurrencySelect1.value == "libra") {
         pCurrencyName1.innerHTML = "Libra Britânica"
         imageCurrencyImg1.src = "./assets/libra.png"
     }
-    if (SelectCurrencySelect1.value == "real") {
+
+    if (selectCurrencySelect1.value == "real") {
         pCurrencyName1.innerHTML = "Real Brasileiro"
         imageCurrencyImg1.src = "./assets/real.png"
+    }
+
+       //                 Segundo Select
+
+    if (selectCurrencySelect2.value == "dolar") {
+        pCurrencyName2.innerHTML = "Dólar Americano"
+        imageCurrencyImg2.src = "./assets/dollar.png"
+    }
+
+    if (selectCurrencySelect2.value == "euro") {
+        pCurrencyName2.innerHTML = "Euro"
+        imageCurrencyImg2.src = "./assets/euro.png"
+    }
+
+    if (selectCurrencySelect2.value == "libra") {
+        pCurrencyName2.innerHTML = "Libra Britânica"
+        imageCurrencyImg2.src = "./assets/libra.png"
+    }
+
+    if (selectCurrencySelect2.value == "real") {
+        pCurrencyName2.innerHTML = "Real Brasileiro"
+        imageCurrencyImg2.src = "./assets/real.png"
     }
     convertCurrency()
    
 }
 
-//           TROCA DO SEGUNDO SELECT
-
-function changeCurrency2() {
-    const pCurrencyName2 = document.getElementById("currency-name2")
-    const imageCurrencyImg2 = document.querySelector(".currency-img2")
-
-    if (SelectCurrencySelect2.value == "dolar") {
-        pCurrencyName2.innerHTML = "Dólar Americano"
-        imageCurrencyImg2.src = "./assets/dollar.png"
-    }
-    if (SelectCurrencySelect2.value == "euro") {
-        pCurrencyName2.innerHTML = "Euro"
-        imageCurrencyImg2.src = "./assets/euro.png"
-    }
-    if (SelectCurrencySelect2.value == "libra") {
-        pCurrencyName2.innerHTML = "Libra Britânica"
-        imageCurrencyImg2.src = "./assets/libra.png"
-    }
-    if (SelectCurrencySelect2.value == "real") {
-        pCurrencyName2.innerHTML = "Real Brasileiro"
-        imageCurrencyImg2.src = "./assets/real.png"
-    }
-    convertCurrency()
-    
-}
-
 btnConvertCurrency.addEventListener("click", convertCurrency)
-SelectCurrencySelect1.addEventListener("change", changeCurrency1)
-SelectCurrencySelect2.addEventListener("change", changeCurrency2)
+selectCurrencySelect1.addEventListener("change", changeCurrency)
+selectCurrencySelect2.addEventListener("change", changeCurrency)
